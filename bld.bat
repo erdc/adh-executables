0fc0e8911c67aa726ec584b195a99f2e9744f771
Joseph Jabour <joseph.e.jabour@erdc.dren.mil>
2019-08-20 13:44:06 -0500

    Removing files from master

--- bld.bat
+++
@@ -1,7 +0,0 @@
-git clone https://github.com/jejabour/adhtestingrevised.git
-git fetch
-git checkout -b adh_v45_win64 origin/adh_v45_win64
-
-REM cd C:\Users\RDITLJEJ\Desktop\full_adh\adhtesting\adh_v45_win64
-python setup.py install --single-version-externally-managed --record=record.txt
-if errorlevel 1 exit 1
