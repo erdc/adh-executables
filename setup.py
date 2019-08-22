919ce435c0d9ab791b551ed5d536939fff87f67c
Joseph Jabour <joseph.e.jabour@erdc.dren.mil>
2019-08-22 10:02:36 -0500

    Adding the files for v5 win64

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='adh_V5.0-WIN64',
+    version='5.0',
+    packages=[],
+    data_files=[('Lib/site-packages/adh_V5.0-WIN64', ['adh_V5.0-WIN64.exe'])],
+    url='https://github.com/jejabour/adhtestingrevised',
+    author='Joseph Jabour',
+    author_email='joseph.e.jabour@erdc.dren.mil',
+    description='Executable for ERDC CHL computational AdH model',
+    install_requires=[]
+)
